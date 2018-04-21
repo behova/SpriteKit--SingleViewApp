7ec5a77c27f0357906946962f1f73b6ff936554f
iMac <imac@imacs-imac.local>
2018-04-21 11:28:22 -0700

    commit 1.1

--- ADVENT_TUT/GameViewController.swift
+++ ADVENT_TUT/GameViewController.swift
@@ -19,7 +19,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("test")
+        
         
         view.addSubview(skView)
         
