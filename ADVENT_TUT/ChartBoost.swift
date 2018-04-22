0ddff58c009ca5274536ef47ca68937739be2e9e
iMac <imac@imacs-imac.local>
2018-04-22 10:18:36 -0700

    skipped chart boost tutorial

---
+++ ADVENT_TUT/ChartBoost.swift
@@ -0,0 +1,10 @@
+//
+//  ChartBoost.swift
+//  ADVENT_TUT
+//
+//  Created by iMac on 4/22/18.
+//  Copyright © 2018 HipsterTrikster. All rights reserved.
+//
+
+import Foundation
+/// I skipped thiss
