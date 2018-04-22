3771ebe39c7b8a73888d75f4b12aaa61f99aec2c
iMac <imac@imacs-imac.local>
2018-04-22 10:31:50 -0700

    Added functions to ACTPlayerStats to keep track of best score and current score

--- ADVENT_TUT/ACTPlayerStats.swift
+++ ADVENT_TUT/ACTPlayerStats.swift
@@ -12,6 +12,8 @@ import SpriteKit
 let kSoundState = "kSoundState"
 let kBackgroundMusic = "backGround"
 let kBackgroundExt = "mp3"
+let kScore = "kScore"
+let kBestScore = "kBestScore"
 
 enum SoundFileName: String {
     case click = "click.mp3"
@@ -21,6 +23,29 @@ enum SoundFileName: String {
 class ACTPlayerStats {
     private init() {}
     static let shared = ACTPlayerStats()
+    
+    func setScore(_ value: Int) {
+        
+        if value > getBestScore() {
+            
+            setBestScore(value)
+        }
+        UserDefaults.standard.set(value, forKey: kScore)
+        UserDefaults.standard.synchronize()
+    }
+    
+    func getScore() -> Int {
+        return UserDefaults.standard.integer(forKey: kScore)
+    }
+    
+    func setBestScore(_ value: Int) {
+        UserDefaults.standard.set(value, forKey: kBestScore)
+        UserDefaults.standard.synchronize()
+    }
+    
+    func getBestScore() -> Int {
+        return UserDefaults.standard.integer(forKey: kBestScore)
+    }
     
     func setSounds(_ state: Bool) {
         UserDefaults.standard.set(state, forKey: kSoundState)
